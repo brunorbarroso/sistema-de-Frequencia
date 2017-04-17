33213c47534f8c9c896f18feeed45d1bf013e3f2
Bruno Barroso Rodrigues <bruno.rodrigues@geq.com.br>
2017-04-17 11:36:43 -0300

    Ativando o calculo de idade

--- app/Support/Helpers.php
+++ app/Support/Helpers.php
@@ -52,7 +52,7 @@ if( !function_exists( 'getImagem' ) ) {
 if( !function_exists( 'getIdade' ) ) {
     
     function getIdade( $datanascimento ){
-        return 0;
+        #return 0;
         $date = new DateTime( $datanascimento ); 
         $interval = $date->diff( new DateTime() );
         return $interval->format( '%Y Anos, %m Meses e %d Dias' );
