5cdc27164cdabdc7d56c82d4624a1d3e254ed0aa
Bruno Barroso Rodrigues <bruno.rodrigues@geq.com.br>
2017-04-27 15:09:57 -0300

    Importando arquivos de CSS e fontes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,11 +8,11 @@
     <title>Ação Social Lumen</title>
 
     <!-- Fonts -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css" integrity="sha384-XdYbMnZ/QjLh6iI4ogqCTaIjrFk87ip+ekIjefZch0Y+PvJ8CDYtEs1ipDmPorQ+" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700">
+    <link rel="stylesheet" href="{{ asset('assets/theme/css/font-awesome.min.css')}}" integrity="sha384-XdYbMnZ/QjLh6iI4ogqCTaIjrFk87ip+ekIjefZch0Y+PvJ8CDYtEs1ipDmPorQ+" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{ asset('assets/theme/css/fonts.css')}}">
 
     <!-- Styles -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{ asset('assets/theme/css/bootstrap.min.css')}}" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
     {{-- <link href="{{ elixir('css/app.css') }}" rel="stylesheet"> --}}
 
     <style>
@@ -123,7 +123,7 @@
 <footer>
     <div class="container">
         <div class="row">        
-            Sistema desenvolvido com muito &hearts; por <a href="http://fb.com/brunobarrosor">Bruno Barroso </a>(namorado da Mari)
+            Sistema desenvolvido com muito &hearts; por <a href="http://fb.com/brunobarrosor">Bruno Barroso </a>
         </div>
         <div class="row">
             © Copyright 2017
